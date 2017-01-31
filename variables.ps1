0a9ed581d7098daf8354ff1faa0301cc17cd793e
Martin Ammerlaan <martin.ammerlaan@gmail.com>
2017-01-31 20:00:50 +0100

    adjusted loopcounter, and reduced waittime

--- variables.ps1
+++ variables.ps1
@@ -13,7 +13,7 @@ $bridgeIP 				= "192.168.178.12"
 $username 				= "d84f5e9696ffd7394edb2d22fa3cfb"
 $ApiUrl 				= "http://$BridgeIP/api/$username/lights"
 $logbasedirectory 		= "\logs\"
-$loopwaitcounterSec		= (Get-Random -Minimum 600 -Maximum 1200)
+$loopwaitcounterSec		= (Get-Random -Minimum 1600 -Maximum 2800)
 $colortransitiontime	= 180
 
 #the device name, and IP of the device you want to have detected. This device must be response to ICMP (ping)
@@ -21,7 +21,7 @@ $devices = @( ("PhoneMart", "192.168.178.98"), ("PhoneSas", "192.168.178.97") )
 #the id's of your HUE lights, get the ID's by calling you HUE api. See variable $ApiUrl
 $huelights = @( ("1"),("2"),("3") )
 
-$waittimeshort = 30
+$waittimeshort = 1
 $script:engagedHue = 0
 $waittimelong = 600
 
